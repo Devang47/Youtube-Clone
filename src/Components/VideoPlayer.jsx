40a8150b8cb719ec3aa1056c770231e9eadcef42
Devang Saklani <72780418+Devang47@users.noreply.github.com>
2021-08-04 20:17:13 +0530

    Description section added

--- src/Components/VideoPlayer.jsx
+++ src/Components/VideoPlayer.jsx
@@ -6,7 +6,9 @@ import ThumbDownIcon from "@material-ui/icons/ThumbDown";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import SubtitlesIcon from "@material-ui/icons/Subtitles";
 import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 import FlagIcon from "@material-ui/icons/Flag";
+import Button from "@material-ui/core/Button";
 
 function VideoPlayer() {
   const [combined, setCombined] = useState({
@@ -21,7 +23,9 @@ function VideoPlayer() {
           {/* <div className="tags">#webdev #js #100SecondsOfCode</div> */}
           <div className="title_date">
             <h1>Avengers Infinity War with Ashish Chanchlani</h1>
-            <div className="date">157,076 views Mar 15, 2021</div>
+            <div className="date">
+              157,076 views <span className="dot"></span> Mar 15, 2021
+            </div>
           </div>
           <div className="details_right">
             <button className="likes">
@@ -39,7 +43,7 @@ function VideoPlayer() {
                 <g>
                   <path
                     d="M14 9V3L22 12L14 21V15C8.44 15 4.78 17.03 2 21C3.11 15.33 6.22 10.13 14 9Z"
-                    class="style-scope yt-icon"
+                    className="style-scope yt-icon"
                   ></path>
                 </g>
               </svg>
@@ -49,19 +53,26 @@ function VideoPlayer() {
               <PlaylistAddIcon />
               SAVE
             </button>
-            <button className="more">
+            <button
+              className="more"
+              onClick={(e) =>
+                setCombined({ ...combined, hiddenMenu: !combined.hiddenMenu })
+              }
+            >
               <MoreHorizIcon />
             </button>
-            <div className="Menu">
-              <button className="item">
-                <FlagIcon />
-                Report
-              </button>
-              <button className="item">
-                <SubtitlesIcon />
-                Open Transcript
-              </button>
-            </div>
+            {combined.hiddenMenu && (
+              <div className="Menu">
+                <button className="item">
+                  <FlagIcon />
+                  Report
+                </button>
+                <button className="item">
+                  <SubtitlesIcon />
+                  Open Transcript
+                </button>
+              </div>
+            )}
           </div>
         </div>
         <div className="channel_details">
@@ -69,33 +80,55 @@ function VideoPlayer() {
             <img src="" alt="" />
           </div>
           <div className="channel_name">
-            <h2>Lorem ipsum</h2>
-            <span className="subscribers">123K</span>
+            <h2>
+              ashish chanchlani vines <CheckCircleIcon />
+            </h2>
+            <span className="subscribers">25.7M subscribers</span>
           </div>
-          <div className="subscribe_btn">Subscribe</div>
+          <div className="subscribe_btn_container">
+            {/* <Button variant="contained" color="red"  className='subscribe_btn'>
+              Subscribe
+            </Button> */}
+            <Button variant="contained" className="subscribed_btn">
+              Subscribed
+            </Button>
+          </div>
           <div className="notifications">
             <NotificationsNoneIcon />
           </div>
         </div>
         <div className="description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi quam in
-          perspiciatis amet dolorum dolore officiis asperiores cumque debitis
-          quasi quod, voluptatem, dignissimos, magnam quae. Praesentium quia
-          doloribus ea ex. Want to build your own peer-to-peer video chat app?
-          WebRTC is a technology that creates a realtime connection between
-          browsers where users can exchange audio/video streams
-          https://fireship.io/lessons/webrtc-fi... 00:00 WebRTC Explained 02:01
-          Build your own Video Chat 3:37 Code setup 04:34 Peer Connection &
-          Webcam 06:49 Offer Signaling 09:45 Answer Signaling Source Code
-          https://github.com/fireship-io/webrtc... Useful Resources WebRTC Docs
-          https://webrtc.org/ Codelab https://webrtc.org/getting-started/fi...
-          Signaling https://developer.mozilla.org/en-US/d... #webdev #js
-          #100SecondsOfCode Install the quiz app 🤓 iOS
-          https://itunes.apple.com/us/app/fires... Android
-          https://play.google.com/store/apps/de... Upgrade to Fireship PRO at
-          https://fireship.io/pro Use code lORhwXd2 for 25% off your first
-          payment. My VS Code Theme - Atom One Dark - vscode-icons - Fira Code
-          Font
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+          <a href="http://localhost:3000/video">
+            {" "}
+            https://www.youtube.com/watch?v=xh1ROLEDyP4&t=242s
+          </a>
+          <br />
+          <br />
+          alias quidem aliquam esse ea, temporibus ipsam inventore itaque error
+          <br />
+          <br />
+          provident autem libero amet? Non, reprehenderit. Aperiam voluptatibus
+          <br />
+          perspiciatis reiciendis deserunt!
+          <br />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit non
+          <br />
+          <br />
+          perferendis ad provident dolorem laboriosam odio amet et dicta, facere
+          <br />
+          repellat nobis similique assumenda, iusto quidem corporis a molestiae
+          iste!
+          <br />
+          <br />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum repellat
+          <br />
+          iste sequi sed minus adipisci totam fuga officia soluta laborum sint
+          <br />
+          repudiandae, ullam officiis aliquid eveniet, quisquam sapiente debitis
+          <br />
+          cum.
+          <br />
         </div>
       </section>
 
